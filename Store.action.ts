42dc831993047f36c27e2f2fccd872ccb18903f6
cbman <cbman@DESKTOP-TAB76U8>
2023-07-15 15:44:11 +0530

    sdfds

--- Store.action.ts
+++ Store.action.ts
@@ -1,3 +1,4 @@
 import { createAction, props } from "@ngrx/store";
 
 export const event_action = createAction('event',props<{value:any}>())
+export const event_action2 = createAction('event',props<{value:any}>())
